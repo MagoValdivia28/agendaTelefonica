548d8f87916fc13edaed4fdc54f70d3f69871e8b
felipe pedro <felipepedro.senai564@gmail.com>
2023-09-04 08:19:59 -0300

    class contato+getZodiacSign

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+class contato{
+    constructor(name, tell, cell, imagem, data, email, cep, city, insta, github){
+        this.name = name;
+        this.tell = tell;
+        this.cell = cell;
+        this.imagem = imagem;
+        this.data = data;
+        this.email = email;
+        this.cep = cep;
+        this.city = city;
+        this.insta = insta;
+        this.github = github;
+    }
+
+getZodiacSign() {
+    let birthdate = new Date(this.birthdate);
+    let day = birthdate.getDate();
+    let month = birthdate.getMonth() + 1;
+    console.log("Passou pelo getSigno() da class User");
+    if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
+        return "Capricórnio ♑";
+    } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+        return "Aquário ♒";
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        return "Peixes ♓";
+    } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+        return "Áries ♈";
+    } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+        return "Touro ♉";
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        return "Gêmeos ♊";
+    } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+        return "Câncer ♋";
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+        return "Leão ♌";
+    } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+        return "Virgem ♍";
+    } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+        return "Libra ♎";
+    } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+        return "Escorpião ♏";
+    } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+        return "Sagitário ♐";
+    }
+}
+
